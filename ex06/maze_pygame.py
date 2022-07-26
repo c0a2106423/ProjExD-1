13f207178462f3bd83471335fa53890083da09d5
RinShigekane <c0a2106423@edu.teu.ac.jp>
2022-07-26 18:31:29 +0900

    fix #25 #28 #29 #30 #32

--- ex06/maze_pygame.py
+++ ex06/maze_pygame.py
@@ -3,6 +3,7 @@ import pygame as pg
 import sys
 import random as rd
 import maze_maker
+import copy
 
 kuma_dia=0.125 #くまの倍率
 tori_dia=1.5 #鳥の倍率
@@ -11,9 +12,7 @@ tori_dia=1.5 #鳥の倍率
 class Screen:
 
     def __init__(self, x_size, y_size) -> None:
-        
         pg.display.set_caption("こうかとん迷路")
-
         self.x = x_size# 迷路のxマス数
         self.y = y_size# 迷路のyマス数
         self.tile_length = 100 #タイルの大きさ
@@ -22,10 +21,8 @@ class Screen:
         self.rct = self.sfc.get_rect()
 
         self.start_color = (0,0,255)
-
         self.wall_color = (0,100,0)
         self.floor_color = (216,208,104)
-
         self.goal_color = (255,0,0)
         
         self.maze_map = maze_maker.make_maze(self.x,self.y)
@@ -37,7 +34,6 @@ class Screen:
             for j in range(self.x):
                 if self.maze_map[i][j] == 1:
                     color = self.wall_color
-                   
                     pg.draw.rect(
                         surface=self.sfc,
                         color=color,
@@ -48,7 +44,6 @@ class Screen:
             color=self.start_color,
             rect=(1*(self.tile_length+1),1*(self.tile_length+1),(self.tile_length-1),(self.tile_length-1)),
             width=0)
-        
         pg.draw.rect(
             surface=self.sfc,
             color=self.goal_color,
@@ -65,7 +60,6 @@ class Screen:
         
         if len(end_p): #候補が一つでもあれば
             i = rd.randint(0,len(end_p)-1) #リストのインデックスをランダムに選び
-          
             y = end_p[i] # y に代入
         else: #候補がない場合
             for i in range(height): #とりあえず右端から２列目の床のマスにゴールを設定
@@ -89,30 +83,26 @@ class Bird:
         self.scr = scr
         self.tile_size = self.scr.tile_length
 
-        self.xy = 1+1j
-        self.rct.center = (int(self.xy.real)*self.tile_size+(self.rct.size[0]//2),int(self.xy.imag)*self.tile_size+(self.rct.size[1]//2))
+        self.xy = [1,1]
+        self.rct.center = (int(self.xy[0])*self.tile_size+(self.rct.size[0]//2),int(self.xy[1])*self.tile_size+(self.rct.size[1]//2))
 
     def update(self, scr, txt=None):
         global stage_count
-        tmp = self.xy
+        tmp = copy.deepcopy(self.xy)
         key_states = pg.key.get_pressed()  # 辞書
         if key_states[pg.K_UP]:
-            """ self.rct.centery -= 1 """
-            self.xy -= 1j
+            self.xy[1] -= 1
         if key_states[pg.K_DOWN]:
-            """ self.rct.centery += 1 """
-            self.xy += 1j
+            self.xy[1] += 1
         if key_states[pg.K_LEFT]:
-            """ self.rct.centerx -= 1 """
-            self.xy -= 1
+            self.xy[0] -= 1
         if key_states[pg.K_RIGHT]:
-            """ self.rct.centerx += 1 """
-            self.xy += 1
-
-        if scr.maze_map[int(self.xy.imag)][int(self.xy.real)] != 1:
-            self.rct.center = (int(self.xy.real)*self.tile_size+(self.tile_size//2),int(self.xy.imag)*self.tile_size+(self.tile_size//2))
+            self.xy[0] += 1
+        
+        if scr.maze_map[self.xy[1]][self.xy[0]] != 1:
+            self.rct.center = (self.xy[0]*self.tile_size+(self.tile_size//2),self.xy[1]*self.tile_size+(self.tile_size//2))
         else:
-            self.xy = tmp
+            self.xy = copy.deepcopy(tmp)
         
         scr.sfc.blit(self.sfc,self.rct)
 
@@ -120,39 +110,35 @@ class Bird:
 class Bear(Bird):
     def __init__(self, image, size, scr):
         super().__init__(image, size, scr)
-        self.xy = complex(self.scr.goal_pos[0],self.scr.goal_pos[1])
-        self.rct.center = (int(self.xy.real)*self.tile_size+(self.tile_size//2),int(self.xy.imag)*self.tile_size+(self.tile_size//2))
+        self.xy = [self.scr.goal_pos[0],self.scr.goal_pos[1]]
+        self.rct.center = (int(self.xy[0])*self.tile_size+(self.tile_size//2),int(self.xy[1])*self.tile_size+(self.tile_size//2))
         self.now_dir = 0
         self.count = 0
         self.interval = 5
 
     def update(self, scr:Screen):
-        tmp = self.xy
+        tmp = copy.deepcopy(self.xy)
         # クマの行動
         self.count += 1 #重兼修正 左手法実装
         if self.count > self.interval: #既定の間隔で
-            self.now_dir, xy_real, xy_imag = self.search_left(scr.maze_map) 
-            self.xy = complex(xy_real,xy_imag) #くまを動かす
+            self.now_dir, x, y = self.search_left(scr.maze_map) 
+            self.xy = [x,y] #くまを動かす
             self.count = 0
             # 以下は同様
-            if scr.maze_map[int(self.xy.imag)][int(self.xy.real)] != 1:
-                self.rct.center = (int(self.xy.real)*self.tile_size+(self.tile_size//2),int(self.xy.imag)*self.tile_size+(self.tile_size//2))
+            if scr.maze_map[int(self.xy[1])][int(self.xy[0])] != 1:
+                self.rct.center = (int(self.xy[0])*self.tile_size+(self.tile_size//2),int(self.xy[1])*self.tile_size+(self.tile_size//2))
             else:
-                self.xy = tmp
+                self.xy = copy.deepcopy(tmp)
         scr.sfc.blit(self.sfc,self.rct)
 
     def search_left(self, maze_map):#重兼追加 左手法実装
-        dif = ((0,1), (1,0), (0,-1),(-1,0))
+        dif = ((-1,0),(0,-1),(1,0),(0,1))
         for i in range(4):
             dir = (self.now_dir+3+i)%4
             x = (self.rct.centerx//int(self.tile_size) + dif[dir][0])
             y = (self.rct.centery//int(self.tile_size) + dif[dir][1])
             if maze_map[y][x] == 0:
-            
                 break
-            else:
-                
-                pass
         return dir,x,y
 
 
@@ -176,38 +162,36 @@ class Text: #重兼修正
 class main():
     def __init__(self) -> None:
         global stage_count
-        stage_count+=1
+        #stage_count+=1
         running = True
         self.scr = Screen(17,9)
         kkt = Bird("ex06/6.png", tori_dia,self.scr)
         brs = [Bear("ex06/animal_kowai_kuma.png",kuma_dia,self.scr)]
         txt = Text(str(stage_count), self.scr, self.scr.tile_length//2, self.scr.tile_length//2)
         clock = pg.time.Clock()
-
+        interval = 500
         counter = 0
 
-
         while running:
-            # ここにコードを書く
-          
-
-            if counter > 500:
+            # クマの増殖判定
+            if counter > interval//stage_count:
                 brs.append(Bear("ex06/animal_kowai_kuma.png",kuma_dia,self.scr))
                 counter = 0
-
+            #クマ以外の描写関係
             self.scr.draw_map()
             txt.blit(self.scr)
             kkt.update(self.scr, txt)
-            """ brs[0].update(scr) """
+            #クマ関係
+            flg = False
             for b in brs:
                 b.update(self.scr)
-
-            for b in brs:
                 if kkt.rct.colliderect(b.rct):
-                    self.game_over()
-                    return
-            
-            if kkt.xy == complex(self.scr.goal_pos[0],self.scr.goal_pos[1]):
+                    flg = True
+            if flg == True: 
+                self.game_over()
+                return
+            #ゴール判定
+            if kkt.xy == [self.scr.goal_pos[0],self.scr.goal_pos[1]]:
                 self.scr.reset_maze()
                 counter = 0
                 stage_count+=1
@@ -215,28 +199,27 @@ class main():
                 for i in range(len(brs)-1,-1,-1):
                     # print(i)
                     if i == 0:
-                        brs[i].xy = complex(self.scr.goal_pos[0],self.scr.goal_pos[1])
-                        brs[i].rct.center = (int(brs[i].xy.real)*brs[i].tile_size+(brs[i].tile_size//2),int(brs[i].xy.imag)*brs[i].tile_size+(brs[i].tile_size//2))
+                        brs[i].xy = [self.scr.goal_pos[0],self.scr.goal_pos[1]]
+                        brs[i].rct.center = (int(brs[i].xy[0])*brs[i].tile_size+(brs[i].tile_size//2),int(brs[i].xy[1])*brs[i].tile_size+(brs[i].tile_size//2))
                     else:
                         brs.pop(-1)
                         i -= 1
-                kkt.xy = 1+1j
-                kkt.rct.center = (int(kkt.xy.real)*kkt.tile_size+(kkt.tile_size//2),int(kkt.xy.imag)*kkt.tile_size+(kkt.tile_size//2))
+                kkt.xy = [1, 1]
+                kkt.rct.center = kkt.xy[0]*kkt.tile_size+(kkt.tile_size//2),kkt.xy[1]*kkt.tile_size+(kkt.tile_size//2)
 
             # イベント処理部
             for event in pg.event.get():
                 if event.type == pg.QUIT:  # 終了イベント
                     running = False
-                    pg.quit()  #pygameのウィンドウを閉じる
-                    sys.exit() #システム終了    
                     return
-                if event.type == pg.KEYDOWN and event.key == pg.K_r:
+                elif event.type == pg.KEYDOWN and event.key == pg.K_r:
                     main()   
+                    return
 
             pg.display.update() #描画処理を実行
             counter += 1
             clock.tick(10)
-    
+
     def game_over(self):
         if not pg.font.get_init:
             pg.font.init()
@@ -246,15 +229,21 @@ class main():
         self.rct.center = self.scr.rct.width/2,self.scr.rct.height/2
         self.scr.sfc.blit(self.sfc,self.rct) #文言を表示
         pg.display.update()
-        while True: #バッテンが押されるまで無限ループ
+
+        while True: #無限ループ
             for event in pg.event.get():
                 if event.type == pg.QUIT:
+                    return
+                elif event.type == pg.KEYDOWN and event.key == pg.K_r:
+                    global stage_count
+                    stage_count = 1
+                    main()   
                     return
 
 # %%
 if __name__ == '__main__':
     global stage_count
-    stage_count = 0
+    stage_count = 1
     pg.init()
     main()
     pg.quit()
